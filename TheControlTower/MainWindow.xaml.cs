46e567ba3d99338c69c1e24be7c1c33a0d5924c9
Orseji <orgi.sejdini@hotmail.com>
2014-12-13 23:09:54 +0100

    Project start in 3.2.1

---
+++ TheControlTower/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TheControlTower.planes;
+
+namespace TheControlTower
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GridViewColumn gvc = new GridViewColumn();
+            Binding flightCdeBinding = new Binding("flightCode");
+            for (int i = 0; i < 50; i++)
+            {
+                flightDataLst.Items.Add(new Plane { FlightCode = "AFR2122", Status = "On Air", Time = "21" });
+            }
+        }
+
+    }
+}
---
+++ TheControlTower/planes/Plane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheControlTower.planes
+{
+    /// <summary>
+    /// Class plane
+    /// </summary>
+    class Plane
+    {
+        public string FlightCode { get; set; }
+        public string Status { get; set; }
+        public string Time { get; set; }
+    }
+}
