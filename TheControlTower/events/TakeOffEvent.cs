4c5ee4b7b2be258facd31a6f10ef644931bae10f
Orseji <orgi.sejdini@hotmail.com>
2014-12-15 21:25:41 +0100

    das

---
+++ TheControlTower/events/TakeOffEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheControlTower.events
+{
+    public  delegate string ReturnMessage(string message);
+    class TakeOffEvent : EventArgs
+    {
+
+    }
+}
